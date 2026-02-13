019ba275b8c53a406ff4f7aff1c2465b13df2ab4
phup36963-sys <phup36963@gmail.com>
2026-02-13 20:29:39 +0700

    Add files via upload

--- FloatButton.mm
+++ FloatButton.mm
@@ -1,32 +1,53 @@
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
+#import <WebKit/WebKit.h> // Header quan trọng cho HTML
 #import "FloatWindow.h"
 #import "FloatButton.h"
 
-@interface NetPingAction : NSObject
+// --- Interface xử lý Action ---
+@interface NetPingAction : NSObject <WKScriptMessageHandler>
 + (void)handleMove:(UIPanGestureRecognizer *)p;
 @end
 
-static UIWindow *overlayWindow = nil;
-static UIButton *floatButton = nil;
+// --- Biến Static quản lý ---
+static UIView *containerView = nil; // Thay đổi từ UIButton thành UIView để chứa Web
+static WKWebView *menuWebView = nil;
+static BOOL isMenuOpen = NO;
 
 @implementation NetPingAction
+
+// Xử lý di chuyển nút
 + (void)handleMove:(UIPanGestureRecognizer *)p {
-    if (floatButton) {
-        CGPoint t = [p translationInView:floatButton.superview];
-        floatButton.center = CGPointMake(floatButton.center.x + t.x, floatButton.center.y + t.y);
-        [p setTranslation:CGPointZero inView:floatButton.superview];
+    if (containerView) {
+        CGPoint t = [p translationInView:containerView.superview];
+        containerView.center = CGPointMake(containerView.center.x + t.x, containerView.center.y + t.y);
+        [p setTranslation:CGPointZero inView:containerView.superview];
+    }
+}
+
+// Nhận lệnh từ HTML (Bridge)
+- (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message {
+    if ([message.name isEqualToString:@"netping"]) {
+        NSDictionary *data = message.body;
+        NSString *action = data[@"action"];
+        
+        if ([action isEqualToString:@"toggleOverlay"]) {
+            // Xử lý đóng mở menu hoặc các lệnh khác
+            NSLog(@"[NetPing] Action received: %@", action);
+        }
     }
 }
 @end
 
+#pragma mark - Overlay Logic
+
 void createNetPingOverlay(void) {
-    // Nếu đã tồn tại thì không tạo nữa
-    if (floatButton) return;
+    if (containerView) return;
 
     dispatch_async(dispatch_get_main_queue(), ^{
-        // CHIẾN THUẬT MỚI: Thay vì tạo UIWindow riêng, ta bám vào Window có sẵn của Game
         UIWindow *keyWindow = nil;
+        
+        // Cố gắng tìm KeyWindow của Game
         for (UIWindow *window in [UIApplication sharedApplication].windows) {
             if (window.isKeyWindow) {
                 keyWindow = window;
@@ -35,29 +56,61 @@ void createNetPingOverlay(void) {
         }
         
         if (!keyWindow) {
-            // Nếu chưa có KeyWindow, lấy đại cái đầu tiên trong Scene
             UIWindowScene *scene = (UIWindowScene *)[[UIApplication sharedApplication].connectedScenes anyObject];
             keyWindow = scene.windows.firstObject;
         }
 
         if (keyWindow) {
-            floatButton = [UIButton buttonWithType:UIButtonTypeCustom];
-            floatButton.frame = CGRectMake(100, 200, 60, 60);
-            floatButton.backgroundColor = [[UIColor systemRedColor] colorWithAlphaComponent:0.9];
-            floatButton.layer.cornerRadius = 30;
-            floatButton.layer.zPosition = 9999; // Ép lên đỉnh Layer Stack
-            floatButton.layer.borderWidth = 1.5;
-            floatButton.layer.borderColor = [UIColor whiteColor].CGColor;
-            [floatButton setTitle:@"NP" forState:UIControlStateNormal];
+            // 1. Khởi tạo Container (Cái hộp chứa nút và Web)
+            containerView = [[UIView alloc] initWithFrame:CGRectMake(50, 150, 60, 60)];
+            containerView.backgroundColor = [UIColor clearColor];
+            containerView.layer.zPosition = MAXFLOAT; // Ép lên đỉnh cao nhất
 
-            UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:[NetPingAction class] action:@selector(handleMove:)];
-            [floatButton addGestureRecognizer:pan];
+            // 2. Cấu hình WKWebView (Nạp file HTML)
+            WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
+            NetPingAction *handler = [[NetPingAction alloc] init];
+            [config.userContentController addScriptMessageHandler:handler name:@"netping"];
 
-            // Add trực tiếp vào KeyWindow của App để né Sandbox Window riêng
-            [keyWindow addSubview:floatButton];
-            [keyWindow bringSubviewToFront:floatButton];
+            menuWebView = [[WKWebView alloc] initWithFrame:containerView.bounds configuration:config];
             
-            NSLog(@"[NetPing] Overlay Injected into KeyWindow (iOS 18.4.1 Bypass)");
+            // --- THIẾT LẬP VƯỢT RÀO CẢN MÀU ĐEN ---
+            menuWebView.opaque = NO; // Trong suốt
+            menuWebView.backgroundColor = [UIColor clearColor];
+            menuWebView.scrollView.backgroundColor = [UIColor clearColor];
+            menuWebView.scrollView.bounces = NO; // Tắt kéo dãn
+            
+            // Bypass Safe Area (Xóa bỏ mảng đen tai thỏ/đáy màn hình)
+            if (@available(iOS 11.0, *)) {
+                menuWebView.scrollView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+            }
+            
+            // Nạp nội dung HTML
+            NSString *htmlPath = [[NSBundle mainBundle] pathForResource:@"index" ofType:@"html"];
+            if (htmlPath) {
+                [menuWebView loadFileURL:[NSURL fileURLWithPath:htmlPath] allowingReadAccessToURL:[NSURL fileURLWithPath:[htmlPath stringByDeletingLastPathComponent]]];
+            } else {
+                // Nếu không có file, hiện chữ NP tạm thời
+                UILabel *label = [[UILabel alloc] initWithFrame:containerView.bounds];
+                label.text = @"NP";
+                label.textAlignment = NSTextAlignmentCenter;
+                label.textColor = [UIColor whiteColor];
+                label.backgroundColor = [[UIColor systemRedColor] colorWithAlphaComponent:0.9];
+                label.layer.cornerRadius = 30;
+                label.clipsToBounds = YES;
+                [containerView addSubview:label];
+            }
+
+            [containerView addSubview:menuWebView];
+
+            // 3. Gesture di chuyển
+            UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:[NetPingAction class] action:@selector(handleMove:)];
+            [containerView addGestureRecognizer:pan];
+
+            // 4. Ép vào Window chính
+            [keyWindow addSubview:containerView];
+            [keyWindow bringSubviewToFront:containerView];
+            
+            NSLog(@"[NetPing] HTML Menu Injected & Black Bars Fixed.");
         }
     });
 }
@@ -65,18 +118,17 @@ void createNetPingOverlay(void) {
 // Constructor tự động chạy
 __attribute__((constructor))
 static void initialize() {
-    // iOS 18.4.1 quét rất gắt lúc load app, phải delay ít nhất 8-10 giây để "vượt rào"
+    // Delay 10s để tránh bị hệ thống quét Window lúc khởi động
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         createNetPingOverlay();
         
-        // Tạo một vòng lặp kiểm tra để đảm bảo nút luôn nổi nếu app vẽ đè
-        NSTimer *keepAlive = [NSTimer scheduledTimerWithTimeInterval:5.0 repeats:YES block:^(NSTimer * _Nonnull timer) {
-            if (floatButton && floatButton.superview) {
-                [floatButton.superview bringSubviewToFront:floatButton];
+        // Vòng lặp cưỡng bức (Keep-Alive)
+        [NSTimer scheduledTimerWithTimeInterval:3.0 repeats:YES block:^(NSTimer * _Nonnull timer) {
+            if (containerView && containerView.superview) {
+                [containerView.superview bringSubviewToFront:containerView];
             } else {
                 createNetPingOverlay();
             }
         }];
-        [[NSRunLoop mainRunLoop] addTimer:keepAlive forMode:NSRunLoopCommonModes];
     });
 }
